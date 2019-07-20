be4ab0897fd34b230acb99e74e38eb937a690b04
pangxiaobin <1456819312@qq.com>
2019-07-20 15:12:58 +0800

    添加爬取失败后再次尝试爬取

--- helper.py
+++ helper.py
@@ -5,12 +5,35 @@
 # @File    : helper.py
 # @Software: PyCharm
 
+import time
 import requests
 from requests.exceptions import ConnectionError
 
 from settings import BASE_HEADERS
 
 
+def retry(max_tries=3, wait=5):
+    """
+    获取失败，进行再次爬取
+    :param max_tries: 失败次数
+    :param wait: 每次失败时等待时间
+    :return:
+    """
+    def deco(fun):
+        def wrapper(*args, **kwargs):
+            for i in range(max_tries):
+                result = fun(*args, **kwargs)
+                if result is None:
+                    print('retry%s' %i)
+                    time.sleep(wait)
+                    continue
+                else:
+                    return result
+        return wrapper
+    return deco
+
+
+@retry()
 def get_text(url, options={}):
     """
     :param method: 请求方法
